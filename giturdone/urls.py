8b65e455b60772712c4ee90244699710ad250aaa
isaacrael <grael@comcast.net>
2015-10-13 21:20:36 -0600

    corrected the quiz url

--- giturdone/urls.py
+++ giturdone/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'giturdone_quiz.views.index', name='index'),
-    url(r'^quiz/', 'giturdone_quiz.views.quiz', name='quiz'),
+    url(r'^quiz/', 'giturdone_quiz.views.git_quiz', name='git_quiz'),
     url(r'^account/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
