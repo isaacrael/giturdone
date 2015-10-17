35f9ff6a4934c1f3c4134a27176da12735b6266d
isaacrael <grael@comcast.net>
2015-10-17 16:24:48 -0600

    added additional views

--- giturdone_quiz/views.py
+++ giturdone_quiz/views.py
@@ -1,8 +1,55 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from .models import Answer, Question
+
 
 # Create your views here.
+
 def index(request):
     return render(request, 'index.html')
 
+
 def git_quiz(request):
-    return render(request, 'git_quiz.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'giturdone_quiz/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'giturdone_quiz/detail.html', {'question': question})
+
+
+def answer(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Answer.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'giturdone_quiz/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.answers += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('giturdone_quiz:results', args=(p.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'giturdone_quiz/results.html', {'question': question})
+
+
+
+"""
+def git_quiz(request):
+    return render(request, 'giturdone_quiz/index.html')
+"""
