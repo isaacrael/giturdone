c0cf4ee71d045f36b12711293f52d50afa13cc41
isaacrael <grael@comcast.net>
2015-10-17 16:27:47 -0600

    added a namespace

--- giturdone/urls.py
+++ giturdone/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
+# Include namespace in the url's
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^quiz/', include('giturdone_quiz.urls', namespace="giturdone_quiz")),
     url(r'^$', 'giturdone_quiz.views.index', name='index'),
-    url(r'^quiz/', 'giturdone_quiz.views.git_quiz', name='git_quiz'),
+#    url(r'^quiz/', 'giturdone_quiz.views.git_quiz', name='git_quiz'),
     url(r'^account/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
 ]
+
