b7ab467965c0733c62bbade1cd96d7a6101ba5bb
grael@comcast.net <cjgtsm79!>
2015-11-06 16:22:54 -0700

    uncommented old results url commented out new results url

--- giturdone_quiz/urls.py
+++ giturdone_quiz/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     # ex: /quiz/
     url(r'^$', views.git_quiz, name='index'),
     url(r'^resources/', views.git_resources, name='resources'),
-    url(r'^results/', views.results, name='results'),
+#    url(r'^results/', views.results, name='results'),
 #    url(r'^quiz/', views.index, name='index'),
     # ex: /quiz/5/
 #    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /quiz/5/results/
-#    url(r'^(?P<question_id>[0-9]+)results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /quiz/5/vote/
 #    url(r'^(?P<question_id>[0-9]+)/answer/$', views.answer, name='answer'),
 ]
