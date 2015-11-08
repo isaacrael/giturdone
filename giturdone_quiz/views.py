6d3fffc007de303e6a9f6e5342b85a5602812c94
isaacrael <grael@comcast.net>
2015-11-08 16:00:41 -0700

    added some code to the results view

--- giturdone_quiz/views.py
+++ giturdone_quiz/views.py
@@ -7,15 +7,28 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from . models import Answer, Question
 import random
-
+import datetime
 
 # Create your views here.
+
+# This context processor function takes the request
+# object and returns a dictionary of context variables
+# to be made available to all templates.
+
+def get_current_time(request):
+  # Create a 'context' dictionary,
+  # populate it with the current time
+  # and return it
+  context = {}
+  context['current_time'] = datetime.datetime.now()
+  return context
+
 
 
 def index(request):
     return render(request, 'index.html')
 
-
+"""
 def git_quiz(request):
     questions = list(Question.objects.all())
     question = random.shuffle(questions)
@@ -24,13 +37,27 @@ def git_quiz(request):
 #    questions = list(question_answer.keys())
 #    question = random.choice(questions)
     return render(request, 'giturdone_quiz/index.html', context)
+"""
+
+
+def git_quiz(request):
+    latest_question_list = Question.objects.all()[:1]
+#    latest_question_list = random.shuffle(temp_latest_question_list)
+    context = {'latest_question_list': latest_question_list}
+#    questions = list(question_answer.keys())
+#    question = random.choice(questions)
+    return render(request, 'giturdone_quiz/index.html', context)
+
+
+
+
 
 def git_resources(request):
         return render(request, 'giturdone_quiz/resources.html')
 
 
 
-"""
+
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -55,7 +82,7 @@ def answer(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('giturdone_quiz:results', args=(p.id,)))
 
-"""
+
 
 """
 def results(request, question_id):
@@ -63,12 +90,25 @@ def results(request, question_id):
     return render(request, 'giturdone_quiz/results.html', {'question': question})
 """
 
+
 def results(request, question_id):
+    latest_question_list = Question.objects.order_by('?')[:2]
+
     if request.method == 'POST':
         question = get_object_or_404(Question, pk=question_id)
         user_answer = request.POST.get('textfield', None)
+        str(user_answer)
+        # correct_answer = answer.correct_answer
+        context = {'latest_question_list': latest_question_list, 'answer': user_answer, 'question': question}
         value = "gil"
-    return render(request, 'giturdone_quiz/results.html', {'answer': user_answer}, {'question': question})
+    return render(request, 'giturdone_quiz/results.html', context)
 
 
+"""
+def results(request):
+    if request.method == 'POST':
+        user_answer = request.POST.get('textfield', None)
+        value = "gil"
+    return render(request, 'giturdone_quiz/results.html', {'answer': user_answer})
+"""
 
