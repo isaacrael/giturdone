04225560d4fb0870ef22ec27773aaec57636865c
isaacrael <grael@comcast.net>
2015-11-13 10:02:25 -0700

    cleaned up file for production version

--- giturdone/urls.py
+++ giturdone/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^quiz/', include('giturdone_quiz.urls', namespace="giturdone_quiz")),
     url(r'^$', 'giturdone_quiz.views.index', name='index'),
     url(r'^results/', 'giturdone_quiz.views.results', name='results'),
+# Left line below in prod version for now just in case it is needed
 #    url(r'^quiz/', 'giturdone_quiz.views.git_quiz', name='git_quiz'),
     url(r'^account/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
