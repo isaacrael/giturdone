230b229e19430dd705490ab339b18aebac6c8b2c
isaacrael <grael@comcast.net>
2015-11-13 17:22:49 -0700

    removed some lines of code that had been commented out

--- giturdone_quiz/urls.py
+++ giturdone_quiz/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     # ex: /quiz/
     url(r'^$', views.git_quiz, name='index'),
     url(r'^resources/', views.git_resources, name='resources'),
-#    url(r'^results/', views.results, name='results'),
-#    url(r'^quiz/', views.index, name='index'),
     # ex: /quiz/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /quiz/5/results/
@@ -18,19 +16,3 @@ urlpatterns = [
 ]
 
 
-"""
-
-urlpatterns = [
-    # ex: /polls/
-    url(r'^$', views.index, name='index'),
-    url(r'^/results/', views.results, name='results'),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-#    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-]
-
-
-"""
