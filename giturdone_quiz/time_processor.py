c770059cecf90f0891b47f184645b77fa7e40002
isaacrael <grael@comcast.net>
2018-01-23 19:54:44 -0700

    processors file for the apps custom_processors

--- giturdone_quiz/time_processor.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-def get_current_time(request):
-  # Create a 'context' dictionary,
-  # populate it with the current time
-  # and return it
-  context = {}
-  context['current_time'] = datetime.datetime.now()
-  return context
