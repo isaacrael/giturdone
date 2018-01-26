f793de12775fa31bed68fcc4218fc34699eae869
isaacrael <grael@comcast.net>
2018-01-25 18:50:24 -0700

    added documentation and imported results from views in an attempt to create a score calculator where dictionary is available to all templates

--- giturdone_quiz/processors.py
+++ giturdone_quiz/processors.py
@@ -1,6 +1,19 @@
 import datetime
 
+# imports results from views in an attempt to create score calculator
+
+from views import results
+
+# This context processor function takes the request
+# object and returns a dictionary of context variables
+# to be made available to all templates.
+
+
 def custom_processor(request):
     now = datetime.datetime.now()
     #welcome_message = "Welcome My Friends! ------"
     return {'DATE_TIME':now}
+
+
+#def score_calculator(request):
+#    return {}
