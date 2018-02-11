87c9dc78c5cc5c311e3b5090192c575e7e20e1f8
isaacrael <grael@comcast.net>
2018-02-11 16:49:57 -0700

    added urls for feynman_technique_quiz details and results pages

--- giturdone_quiz/urls.py
+++ giturdone_quiz/urls.py
@@ -14,8 +14,10 @@ urlpatterns = [
     # ex: /quiz/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     url(r'^reset_scores/', views.reset_scores, name='reset_scores'),
-url(r'^short_answer_quiz_categories/', 'giturdone_quiz.views.short_answer_quiz_categories', name='short_answer_quiz_categories'),
+    url(r'^short_answer_quiz_categories/', 'giturdone_quiz.views.short_answer_quiz_categories', name='short_answer_quiz_categories'),
     url(r'^feynman_technique_quiz/', views.feynman_technique_quiz, name='feynman_technique_quiz'),
+    url(r'^feynman_technique_quiz/(?P<question_id>[0-9]+)/$', views.ftq_detail, name='ftq_detail'),
+    url(r'^feynman_technique_quiz/(?P<question_id>[0-9]+)/results/$', views.ftq_results, name='ftq_results'),
     url(r'^multiple_choice_quiz/', views.feynman_technique_quiz, name='multiple_choice_quiz'),
     # ex: /quiz/5/vote/
 # Note: the answers url is not being used in the app but is left here
