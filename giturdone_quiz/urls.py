3887c841d8e377a09cd7f3dcadf02b0c44c1561f
isaacrael <grael@comcast.net>
2018-02-12 19:10:46 -0700

    added some FTQ url's

--- giturdone_quiz/urls.py
+++ giturdone_quiz/urls.py
@@ -15,10 +15,11 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     url(r'^reset_scores/', views.reset_scores, name='reset_scores'),
     url(r'^short_answer_quiz_categories/', 'giturdone_quiz.views.short_answer_quiz_categories', name='short_answer_quiz_categories'),
-    url(r'^feynman_technique_quiz/', views.feynman_technique_quiz, name='feynman_technique_quiz'),
-    url(r'^feynman_technique_quiz/(?P<question_id>[0-9]+)/$', views.ftq_detail, name='ftq_detail'),
-    url(r'^feynman_technique_quiz/(?P<question_id>[0-9]+)/results/$', views.ftq_results, name='ftq_results'),
-    url(r'^multiple_choice_quiz/', views.feynman_technique_quiz, name='multiple_choice_quiz'),
+    url(r'^feynman_technique_quiz/$', views.feynman_technique_quiz, name='feynman_technique_quiz'),
+    url(r'^feynman_technique_quiz/(?P<question_id>[0-9]+)/$', 'giturdone_quiz.views.ftq_detail', name='ftq_detail'),
+    url(r'^feynman_technique_quiz/(?P<question_id>[0-9]+)/results/$', 'giturdone_quiz.views.ftq_results', name='ftq_results'),
+    url(r'^feynman_technique_quiz/reset_scores/$', views.ftq_reset_scores, name='ftq_reset_scores'),
+    url(r'^multiple_choice_quiz/', views.multiple_choice_quiz, name='multiple_choice_quiz'),
     # ex: /quiz/5/vote/
 # Note: the answers url is not being used in the app but is left here
 # as an example for future versions
