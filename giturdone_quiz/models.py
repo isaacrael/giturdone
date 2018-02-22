4dfee028b1c337aebcbdbc4595e36151ff1c97ee
isaacrael <grael@comcast.net>
2018-02-22 12:36:45 -0800

    added Mc_Question & Mc_Answer models

--- giturdone_quiz/models.py
+++ giturdone_quiz/models.py
@@ -59,3 +59,24 @@ class Ftq_Answer(models.Model):
     def __unicode__(self):
         return self.answer_text
 
+
+class Mc_Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+class Mc_Answer(models.Model):
+    question = models.ForeignKey(Mc_Question)
+    answer_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.answer_text
