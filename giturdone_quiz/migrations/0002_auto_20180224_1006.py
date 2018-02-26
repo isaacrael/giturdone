ee21dce6d0d47fc3275d50f3a0d27bf6e08249e2
isaacrael <grael@comcast.net>
2018-02-25 20:49:05 -0700

    latest migration

--- giturdone_quiz/migrations/0002_auto_20180224_1006.py
+++ giturdone_quiz/migrations/0002_auto_20180224_1006.py
@@ -7,12 +7,17 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('giturdone_quiz', '0008_auto_20180129_1345'),
+        ('giturdone_quiz', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='answer',
+            model_name='mc_answer',
+            name='correct_answer',
+            field=models.CharField(default=b'Linus Torvalds', max_length=200),
+        ),
+        migrations.AddField(
+            model_name='mc_answer',
             name='image',
             field=models.ImageField(upload_to=b'post_image', blank=True),
         ),
