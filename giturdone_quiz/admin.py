c766bd7f93a95322719f07dcbd5176b130205983
isaacrael <grael@comcast.net>
2018-02-27 13:58:38 -0800

    added documentation

--- giturdone_quiz/admin.py
+++ giturdone_quiz/admin.py
@@ -70,13 +70,19 @@ admin.site.register(Answer)
 
 admin.site.register(Question)
 
-# Note: the two lines below register the Answer and Question classes
-# so that Question & Answers are visible together in the admin site
-# making Question & Answer input and deletion very fast
+# Note: the two lines below register the Ftq_Question and Ftq_Question classes
+# so that Ftq_Question & Ftq_Answers are visible together in the admin site
+# making Ftq_Question & Ftq_Answer input and deletion very fast
 
 admin.site.register(Ftq_Answer)
 
 admin.site.register(Ftq_Question)
+
+
+# Note: the two lines below register the Mc_Question and Mc_Question classes
+# so that Mc_Question & Mc_Answers are visible together in the admin site
+# making Mc_Question & Mc_Answer input and deletion very fast
+
 
 
 admin.site.register(Mc_Answer)
