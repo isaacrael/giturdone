c8318cabf8bb4628b6bd165e9842aa1202dcb402
isaacrael <grael@comcast.net>
2018-03-06 17:56:06 -0700

    added some comments and deleted print statement in the short_answer_quiz_graph page

--- giturdone_quiz/views.py
+++ giturdone_quiz/views.py
@@ -36,7 +36,7 @@ def index(request):
     return render(request, 'index.html')
 
 # The following functions are for the Short Answer Quiz
-# quiz_selection page takes a user_response as input and displays associated quiz on Sensei Quiz page
+# quiz_selection page takes a user_response as input and writes the response to giturdone_quiz/user_response.py
 
 def quiz_selection(request):
     if request.method == 'POST':
@@ -48,20 +48,15 @@ def quiz_selection(request):
         f.close()
     return render(request, 'giturdone_quiz/quiz_selection.html')
 
+# The function below will take the short answer quiz category user response and display
+# the questions for the selected category
 
-# git_quiz displays the quiz selected by user in the Quiz Selection page
-
-#def git_quiz(request):
 def quiz(request):
     if request.method == 'POST':
         user_response = request.POST.get('textfield', None)
         user_response = smart_text(user_response)
-#        f = open('giturdone_quiz/user_response.py', 'w')
-#        # the line below write the text 'user_response = ' and concats the user_response the str function gets rid of u in front of string
-#        f.write(str(user_response))
-#        f.close()
         f = open('giturdone_quiz/user_response.py', 'w')
-        # the line below write the text 'user_response = ' and concats the user_response the str function gets rid of u in front of string
+        # the line below writes the text 'user_response = ' and concats the user_response the str function gets rid of u in front of string
         f.write('user_response = ' + repr(str(user_response)))
         f.close()
     else:
@@ -178,19 +173,6 @@ def results(request, question_id):
             Grade = "D"
         elif score in range(0,59):
             Grade = "F"
-        new_answers = Answer.objects.all()
-        f = open('giturdone_quiz/total_correct_answers_field.py', 'w')
-        # the line below write the text 'user_response = ' and concats the user_response the str function gets rid of u in front of string
-        """
-        for answer in new_answers:
-            my_dict = {}
-            my_dict['My_Answer'] = answer.answer_text
-            f.write(str(my_dict['My_Answer']))
-            my_dict['My_Total_Correct_Answers'] = answer.total_correct_answers
-            str(my_dict)
-            f.write(my_dict['My_Total_Correct_Answers'])
-        f.close()
-        """
         context = {'latest_question_list': latest_question_list, 'answer': user_answer,
         'question': question, 'correct_answer': correct_answer, 'Grade':Grade,
         'score':score, 'total_questions_answered': ttl_questions_answered,
@@ -224,13 +206,9 @@ def short_answer_quiz_graphs(request):
     for answer in wrong_answers_ordered:
         count = count + 1
     total = count
-    #    print(total)
-    #    print(count)
     # assign last and first elements in range
     last_element_in_range = total
     first_element_in_range = last_element_in_range - 6
-    #    print(first_element_in_range)
-    #    print(last_element_in_range)
     wrong_answers_ordered = Answer.objects.order_by('total_wrong_answers')
     wrong_answers_ordered = wrong_answers_ordered.values()
     wrong_answers_ordered = wrong_answers_ordered.order_by("total_wrong_answers")
@@ -241,67 +219,35 @@ def short_answer_quiz_graphs(request):
         wrong_answers_ordered = Answer.objects.order_by('total_wrong_answers')
         wrong_answers_ordered = wrong_answers_ordered.values()
         my_dict = wrong_answers_ordered[index]
-        #        print(my_dict)
         id = my_dict["question_id"]
         q = Question.objects.get(id=id)
-        #        print(q)
-        #        print my_dict["answer_text"]
-        #        print my_dict["total_wrong_answers"]
         if index == first_element_in_range:
             question1 = Question.objects.get(pk=id)
             question1_total_wrong_answers = my_dict["total_wrong_answers"]
-            #            print(question1)
-            #            print(question1_total_wrong_answers)
             context["question1"] = question1
             context["question1_total_wrong_answers"] = question1_total_wrong_answers
-        #            print context["question1"]
-        #            print context["question1_total_wrong_answers"]
-        #            print(context)
         elif index == first_element_in_range + 1:
             question2 = Question.objects.get(pk=id)
             question2_total_wrong_answers = my_dict["total_wrong_answers"]
             context["question2"] = question2
             context["question2_total_wrong_answers"] = question2_total_wrong_answers
-        #            print context["question2"]
-        #            print context["question2_total_wrong_answers"]
-        #            print(context)
-        #            print(question2)
-        #            print(question2_total_wrong_answers)
         elif index == first_element_in_range + 2:
             question3 = Question.objects.get(pk=id)
             question3_total_wrong_answers = my_dict["total_wrong_answers"]
             context["question3"] = question3
             context["question3_total_wrong_answers"] = question3_total_wrong_answers
-        #            print context["question3"]
-        #            print context["question3_total_wrong_answers"]
-        #            print(context)
-        #            print(question3)
-        #            print(question3_total_wrong_answers)
         elif index == first_element_in_range + 3:
             question4 = Question.objects.get(pk=id)
             question4_total_wrong_answers = my_dict["total_wrong_answers"]
             context["question4"] = question4
             context["question4_total_wrong_answers"] = question4_total_wrong_answers
-        #            print context["question4"]
-        #            print context["question4_total_wrong_answers"]
-        #            print(context)
-        #            print(question4)
-        #            print(question4_total_wrong_answers)
         elif index == first_element_in_range + 4:
             question5 = Question.objects.get(pk=id)
             question5_total_wrong_answers = my_dict["total_wrong_answers"]
             context["question5"] = question5
             context["question5_total_wrong_answers"] = question5_total_wrong_answers
-        #            print context["question5"]
-        #            print context["question5_total_wrong_answers"]
-        #            print(context)
-        #            print(question5)
-        #            print(question5_total_wrong_answers)
-
-        #        print(index)
         if index != first_element_in_range + 5:
             index = index + 1
-    #        print(context)
     return render(request, 'giturdone_quiz/short_answer_quiz_graphs.html', context)
 
 # The following function renders the Tools Page
